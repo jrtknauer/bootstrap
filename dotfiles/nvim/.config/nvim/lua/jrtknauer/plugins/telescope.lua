75edc3208004f087c3851eed22136a4a75799e1e
Jarrett Knauer <jrtknauer@proton.me>
2024-12-29 21:31:32 -0700

    refactor(telescope): use init instead of config
    Prefer `opts` to `config` with lazy.nvim plugin specifications.

--- dotfiles/nvim/.config/nvim/lua/jrtknauer/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/jrtknauer/plugins/telescope.lua
@@ -23,18 +23,24 @@ return {
 			fzf = {},
 		},
 	},
-	config = function()
+	init = function()
 		require("telescope").load_extension("fzf")
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>pws", function()
-			local word = vim.fin.expand("<cword>")
-			builtin.grep_string({ search = word })
-		end)
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Search files" })
+		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search git files" })
+		vim.keymap.set("n", "<leader>vh", builtin.help_tags, { desc = "Search help tags" })
+
+        local grep_selected = function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end
+        vim.keymap.set("n", "<leader>pws", grep_selected, { desc = "Grep files for currently selected word" })
+
+        local grep_word = function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end
+        vim.keymap.set("n", "<leader>pws", grep_word, { desc = "Grep files for currently selected word" })
+
 	end,
 }

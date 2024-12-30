496b309f5afebc5bea0bc58bbdf1b20dc1d65700
Jarrett Knauer <jrtknauer@proton.me>
2024-12-29 21:34:19 -0700

    style: correct lua formatting per stylua

--- dotfiles/nvim/.config/nvim/lua/jrtknauer/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/jrtknauer/plugins/telescope.lua
@@ -5,20 +5,20 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	opts = {
-        pickers = {
-            find_files = {
-                theme = "ivy",
-            },
-            git_files = {
-                theme = "ivy",
-            },
-            grep_string = {
-                theme = "ivy",
-            },
-            help_tags = {
-                theme = "ivy",
-            },
-        },
+		pickers = {
+			find_files = {
+				theme = "ivy",
+			},
+			git_files = {
+				theme = "ivy",
+			},
+			grep_string = {
+				theme = "ivy",
+			},
+			help_tags = {
+				theme = "ivy",
+			},
+		},
 		extensions = {
 			fzf = {},
 		},
@@ -31,16 +31,15 @@ return {
 		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search git files" })
 		vim.keymap.set("n", "<leader>vh", builtin.help_tags, { desc = "Search help tags" })
 
-        local grep_selected = function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end
-        vim.keymap.set("n", "<leader>pws", grep_selected, { desc = "Grep files for currently selected word" })
+		local grep_selected = function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end
+		vim.keymap.set("n", "<leader>pws", grep_selected, { desc = "Grep files for currently selected word" })
 
-        local grep_word = function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end
-        vim.keymap.set("n", "<leader>pws", grep_word, { desc = "Grep files for currently selected word" })
-
+		local grep_word = function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end
+		vim.keymap.set("n", "<leader>pws", grep_word, { desc = "Grep files for currently selected word" })
 	end,
 }

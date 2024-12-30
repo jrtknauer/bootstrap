5be62449385f970e7f206720b44df80d33c1ce35
Jarrett Knauer <jrtknauer@proton.me>
2024-12-29 18:09:31 -0700

    fix(telescope): correctly install and use fzf

--- dotfiles/nvim/.config/nvim/lua/jrtknauer/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/jrtknauer/plugins/telescope.lua
@@ -2,9 +2,15 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-fzf-native.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
-	init = function()
+	opts = {
+		extensions = {
+			fzf = {},
+		},
+	},
+	config = function()
+		require("telescope").load_extension("fzf")
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})

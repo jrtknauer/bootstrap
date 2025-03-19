a7c435117677af17c5cc7e67705d998fe411e8cc
Jarrett Knauer <jrtknauer@proton.me>
2025-03-19 11:43:02 -0600

    feat(work): add packaging dependencies

--- install/canonical/install.sh
+++ install/canonical/install.sh
@@ -9,6 +9,7 @@ function install_cpc_packages() {
         debhelper \
         devscripts \
         git-build-recipe \
+        petname \
         python3-swiftclient \
         python3-virtualenv \
         qemu-system-x86 \
@@ -19,6 +20,7 @@ function install_cpc_snaps() {
     sudo snap install --classic aws-cli
     sudo snap install --classic gitlptools
     sudo snap install --classic lpshipit
+    sudo snap install --classic snapcraft
 
     # Requires `newgrp lxd` outside of script.
     sudo snap install lxd
